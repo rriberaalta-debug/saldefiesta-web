493e828a7234a23ef7a658ed6766884dfe95fb34
Iñaki <r.riberaalta@gmail.com>
2025-10-19 17:46:26 +0200

    Fix: export app in firebase.ts and corrected storageBucket

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBMSu8A7v_vZSlywHsEZsTkMX2eJJmEpM",
   authDomain: "saldefiesta-appp.firebaseapp.com",
   projectId: "saldefiesta-appp",
-  storageBucket: "saldefiesta-appp.firebasestorage.app",
+  storageBucket: "saldefiesta-appp.appspot.com",
   messagingSenderId: "698613369041",
   appId: "1:698613369041:web:86264aee6ab547b43e9df0"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
+export const storage = getStorage(app);
